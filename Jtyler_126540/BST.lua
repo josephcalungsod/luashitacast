ebf7e33b6f43b9a1750744064c8b80a33fa80158
schmoe <joseph.calungsod@gmail.com>
2026-04-24 22:12:01 -0500

    pass max hp idle w regen in dodefault

--- Jtyler_126540/BST.lua
+++ Jtyler_126540/BST.lua
@@ -217,7 +217,7 @@ profile.HandleCommand = function(args)
 end
 
 profile.HandleDefault = function()
-    gcmelee.DoDefault()
+    gcmelee.DoDefault(max_hp_in_idle_with_regen_gear_equipped)
 
     local player = gData.GetPlayer()
     if (player.Status == 'Idle' and player.HPP < 50 and muscle_belt ~= '') then
