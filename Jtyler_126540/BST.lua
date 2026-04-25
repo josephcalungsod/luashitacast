00a5aa4082231e86e32223f5d545e274d470b33b
schmoe <joseph.calungsod@gmail.com>
2026-04-24 21:59:35 -0500

    bst lua max hp in idle w regen 0

--- Jtyler_126540/BST.lua
+++ Jtyler_126540/BST.lua
@@ -2,7 +2,9 @@ local profile = {}
 
 local fastCastValue = 0.00 -- 0% from gear
 
-local gaudy_harness = true
+local gaudy_harness = false
+
+local max_hp_in_idle_with_regen_gear_equipped = 0 -- You could set this to 0 if you do not wish to ever use regen gear
 
 -- Replace these with '' if you do not have them
 local muscle_belt = ''
